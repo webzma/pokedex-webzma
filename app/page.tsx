9e1c6c4c7bde16fa62185bb0111bb8a65b4b79a3
Wilberk <ledezma.wilberk@gmail.com>
2024-11-15 18:34:53 -0400

    Refactor PokeCard component to use PokeTagProps instead of PokeTag

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { PokeSearcher } from "./components/PokeSearcher";
 import { Title } from "./components/Title/Title";
 import { AlphabetFilter } from "./components/AlphabetFilter";
@@ -5,23 +7,66 @@ import { ButtonFilter } from "./components/ButtonFilter";
 import { PokeCard } from "./components/PokeCard";
 import { fetchAllPokemons } from "@/lib/api/pokedex";
 import { Pokemon } from "@/lib/types/types";
+import { useEffect, useState } from "react";
+import { sortPokemonsAZ } from "@/lib/utils/sortPokemonsAZ";
+import { sortPokemonsZA } from "@/lib/utils/sortPokemonZA";
+import {
+  reversedPokemons,
+  sortPokemonsLowest,
+} from "@/lib/utils/sortPokemonsLowest";
+import { sortPokemonsHighest } from "@/lib/utils/sortPokemonsHighest";
 
-export default async function Home() {
-  const pokemons = await fetchAllPokemons();
+type AlphabetFilterType = "a-z" | "z-a" | "lowest" | "highest";
 
+export default function Home() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [alphabetFilter, setAlphabetFilter] = useState<AlphabetFilterType>();
+
+  console.log(pokemons);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchPokemons = await fetchAllPokemons();
+      setPokemons(fetchPokemons.results);
+    };
+
+    fetchData();
+  }, []);
+
+  let filteredPokemons = pokemons;
+
+  if (alphabetFilter === "a-z") {
+    filteredPokemons = sortPokemonsAZ(pokemons);
+  }
+
+  if (alphabetFilter === "z-a") {
+    filteredPokemons = sortPokemonsZA(pokemons);
+  }
+
+  if (alphabetFilter === "lowest") {
+    filteredPokemons = pokemons;
+  }
+
+  if (alphabetFilter === "highest") {
+    filteredPokemons = sortPokemonsHighest(pokemons);
+  }
+
+  if (!alphabetFilter) {
+    filteredPokemons = pokemons;
+  }
   return (
     <div>
       <div className="max-w-[1416px] m-auto mt-12 px-4">
         <Title />
         <PokeSearcher />
 
-        <div className="flex items-center justify-between mt-8">
-          <AlphabetFilter />
+        <div className="flex mt-8 flex-col items-start justify-start gap-y-3 md:flex-row md:justify-between md:items-center">
+          <AlphabetFilter setAlphabetFilter={setAlphabetFilter} />
           <ButtonFilter />
         </div>
 
         <div className="grid xl:grid-cols-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6  gap-y-28 rounded-lg mt-24 justify-items-center">
-          {pokemons.results.map((poke: Pokemon, index: number) => (
+          {filteredPokemons.map((poke: Pokemon, index: number) => (
             <PokeCard pokeUrl={poke.url} key={index} number={index} />
           ))}
         </div>
