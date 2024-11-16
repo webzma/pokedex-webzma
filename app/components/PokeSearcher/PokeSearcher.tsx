b56b6162abb9b6d3a39e86ef6df31765ffe379a1
Wilberk <ledezma.wilberk@gmail.com>
2024-11-16 00:59:43 -0400

    Refactor PokeSearcher component layout for improved structure and responsiveness

--- app/components/PokeSearcher/PokeSearcher.tsx
+++ app/components/PokeSearcher/PokeSearcher.tsx
@@ -11,26 +11,32 @@ export const PokeSearcher: React.FC<PokeSearcherProps> = ({
   search,
 }) => {
   return (
-    <div className="focus-within:outline focus-within:outline-2 focus-within:outline-[#416EDF] w-full bg-white mt-8 gap-x-4 flex items-center p-2 rounded-xl shadow-2xl shadow-[#C9D2EA]">
-      <div>
-        <Image
-          src="/search-icon.svg"
-          alt="seacrh icon"
-          width={25}
-          height={25}
-          className="ml-2"
+    <div>
+      <div className="focus-within:outline focus-within:outline-2 focus-within:outline-[#416EDF] w-full bg-white mt-8 gap-x-4 flex items-center p-2 rounded-xl shadow-2xl shadow-[#C9D2EA]">
+        <div>
+          <Image
+            src="/search-icon.svg"
+            alt="seacrh icon"
+            width={25}
+            height={25}
+            className="ml-2"
+          />
+        </div>
+
+        <input
+          type="text"
+          className="text-md flex-1 h-10 border-none outline-none text-[#416EDF] font-medium placeholder:text-[#416EDF]"
+          placeholder="Pokemon name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
+
+        <Button className="hidden md:flex px-12 rounded-lg bg-[#FFCE31] text-[#416EDF] text-md hover:bg-[#FFCE31]/90 font-bold">
+          Search
+        </Button>
       </div>
 
-      <input
-        type="text"
-        className="text-md flex-1 h-10 border-none outline-none text-[#416EDF] font-medium placeholder:text-[#416EDF]"
-        placeholder="Pokemon name..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-
-      <Button className="px-12 rounded-lg bg-[#FFCE31] text-[#416EDF] text-md hover:bg-[#FFCE31]/90 font-bold">
+      <Button className="flex md:hidden mt-2 py-6 w-full px-12 rounded-lg bg-[#FFCE31] text-[#416EDF] text-md hover:bg-[#FFCE31]/90 font-bold">
         Search
       </Button>
     </div>
